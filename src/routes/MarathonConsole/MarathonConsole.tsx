c9cdeeb6f8657591f64607b46102606898ff583e
Tag Howard <tag.howard1080@gmail.com>
2022-06-10 23:24:33 -0400

    Add not implemented messages

--- src/routes/MarathonConsole/MarathonConsole.tsx
+++ src/routes/MarathonConsole/MarathonConsole.tsx
@@ -2,6 +2,7 @@ const MarathonConsole = () => {
   return (
     <div>
       <h1>MarathonConsole</h1>
+      <p>This page is not currently implemented</p>
     </div>
   );
 };
--- src/routes/MoraleConsole/MoraleConsole.tsx
+++ src/routes/MoraleConsole/MoraleConsole.tsx
@@ -2,6 +2,7 @@ const MoraleConsole = () => {
   return (
     <div>
       <h1>MoraleConsole</h1>
+      <p>This page is not currently implemented</p>
     </div>
   );
 };

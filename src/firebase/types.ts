e1d46c2307277869238b37d795e992f0436857d5
Tag Howard <tag.howard1080@gmail.com>
2022-06-13 23:05:10 -0400

    Update to reflect new lib repo

--- src/firebase/types.ts
+++
@@ -1,8 +0,0 @@
-// TODO replace this with more specific type definitions
-export interface GenericFirestoreDocument {
-  [key: string]: object | number | string | null | undefined;
-}
-
-export interface GenericFirestoreDocumentWithId extends GenericFirestoreDocument {
-  id: string;
-}

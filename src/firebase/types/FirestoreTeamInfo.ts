9db08e9f1817a7f05d5723e770bc74daf98078b3
Tag Howard <tag.howard1080@gmail.com>
2022-09-16 09:50:01 -0400

    Update Team type

--- src/firebase/types/FirestoreTeamInfo.ts
+++ src/firebase/types/FirestoreTeamInfo.ts
@@ -1,6 +1,8 @@
 export interface FirestoreTeamInfo {
   name: string;
+  teamClass?: "public" | "committee";
   members: string[];
+  memberNames: Record<string, string | null>;
   memberAccounts: Record<string, string | null>;
   fundraisingTotal?: number;
   totalPoints?: number;
@@ -18,11 +20,19 @@ export function isFirestoreTeamInfo(
     return false;
   }
 
+  if (data.teamClass != null && typeof data.teamClass !== "string") {
+    return false;
+  }
+
   if (!Array.isArray(data.members) || data.members.some((m: any) => typeof m !== "string")) {
     return false;
   }
 
   if (typeof data.memberAccounts !== "object" || data.memberAccounts == null) {
+    return false;
+  }
+
+  if (typeof data.memberNames !== "object" || data.memberNames == null) {
     return false;
   }
 

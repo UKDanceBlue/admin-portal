4902cd46b1fb48a66e02eb3902b1e1d558fd464c
Tag Howard <tag.howard1080@gmail.com>
2022-09-16 13:00:09 -0400

    Make the team page more useful

--- src/routes/SpiritConsole/TeamConsole/TeamConsole.tsx
+++ src/routes/SpiritConsole/TeamConsole/TeamConsole.tsx
@@ -1,9 +1,23 @@
+import { Typography } from "@mui/material";
+import { collection, doc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import { useFirestore, useFirestoreDoc } from "reactfire";
+
+import { FirestoreTeamInfo } from "../../../firebase/types/FirestoreTeamInfo";
+
 import PointEntriesDataGrid from "./PointEntriesDataGrid";
+import TeamMembersDataGrid from "./TeamMembersDataGrid";
 
 const TeamConsole = () => {
+  const { teamId } = useParams<{teamId: string}>();
+
+  const firestore = useFirestore();
+  const team = useFirestoreDoc<Partial<FirestoreTeamInfo>>(doc(collection(firestore, "spirit/teams/documents"), teamId));
+
   return (
     <div>
-      <h1>Spirit Point Entries</h1>
+      <Typography variant="h1" textAlign="center">{team.data?.get("name") ?? "Unnamed Spirit Team"}</Typography>
+      <TeamMembersDataGrid teamObservable={team} />
       <PointEntriesDataGrid />
     </div>
   );

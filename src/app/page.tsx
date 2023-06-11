ab335fa915a0a1ca05b556d8747d1d14d16d5068
Tag Howard <tag.howard1080@gmail.com>
2023-06-11 15:25:45 -0400

    Start on style additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,88 @@
+import { Text } from "@/components/core/Text";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
-      <p>Home Page</p>
+      <Text>
+        <div>
+          Arcu odio ut sem nulla pharetra diam. Vitae semper quis lectus nulla
+          at volutpat diam ut venenatis. Non quam lacus suspendisse faucibus
+          interdum. Ac tincidunt vitae semper quis lectus nulla at volutpat
+          diam. Dictum non consectetur a erat nam at lectus urna duis. Id diam
+          vel quam elementum pulvinar etiam non. Fringilla urna porttitor
+          rhoncus dolor purus non enim. Aliquam malesuada bibendum arcu vitae
+          elementum curabitur vitae. Urna id volutpat lacus laoreet non
+          curabitur. Egestas maecenas pharetra convallis posuere morbi leo urna.
+          Tortor consequat id porta nibh venenatis cras sed felis. Ut tristique
+          et egestas quis ipsum. Tristique sollicitudin nibh sit amet commodo
+          nulla facilisi nullam vehicula.
+        </div>
+        <div>
+          Semper eget duis at tellus at urna condimentum mattis. Aenean vel elit
+          scelerisque mauris pellentesque. Eu ultrices vitae auctor eu augue ut
+          lectus. Aliquam etiam erat velit scelerisque in dictum non consectetur
+          a. Eu volutpat odio facilisis mauris. Id aliquet risus feugiat in
+          ante. Eu non diam phasellus vestibulum lorem sed risus ultricies.
+          Magnis dis parturient montes nascetur ridiculus mus mauris vitae. Mi
+          in nulla posuere sollicitudin aliquam ultrices sagittis. In cursus
+          turpis massa tincidunt dui. Cras sed felis eget velit aliquet sagittis
+          id consectetur purus. Velit aliquet sagittis id consectetur purus ut
+          faucibus pulvinar. Convallis convallis tellus id interdum velit
+          laoreet id donec. Neque vitae tempus quam pellentesque nec nam. Semper
+          eget duis at tellus at urna condimentum mattis pellentesque. Faucibus
+          vitae aliquet nec ullamcorper sit. Aliquam vestibulum morbi blandit
+          cursus risus at ultrices. Malesuada nunc vel risus commodo viverra
+          maecenas.
+        </div>
+      </Text>
+      <Text>
+        Arcu odio ut sem nulla pharetra diam. Vitae semper quis lectus nulla at
+        volutpat diam ut venenatis. Non quam lacus suspendisse faucibus
+        interdum. Ac tincidunt vitae semper quis lectus nulla at volutpat diam.
+        Dictum non consectetur a erat nam at lectus urna duis. Id diam vel quam
+        elementum pulvinar etiam non. Fringilla urna porttitor rhoncus dolor
+        purus non enim. Aliquam malesuada bibendum arcu vitae elementum
+        curabitur vitae. Urna id volutpat lacus laoreet non curabitur. Egestas
+        maecenas pharetra convallis posuere morbi leo urna. Tortor consequat id
+        porta nibh venenatis cras sed felis. Ut tristique et egestas quis ipsum.
+        Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam
+        vehicula.
+        <br />
+        Semper eget duis at tellus at urna condimentum mattis. Aenean vel elit
+        scelerisque mauris pellentesque. Eu ultrices vitae auctor eu augue ut
+        lectus. Aliquam etiam erat velit scelerisque in dictum non consectetur
+        a. Eu volutpat odio facilisis mauris. Id aliquet risus feugiat in ante.
+        Eu non diam phasellus vestibulum lorem sed risus ultricies. Magnis dis
+        parturient montes nascetur ridiculus mus mauris vitae. Mi in nulla
+        posuere sollicitudin aliquam ultrices sagittis. In cursus turpis massa
+        tincidunt dui. Cras sed felis eget velit aliquet sagittis id consectetur
+        purus. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.
+        Convallis convallis tellus id interdum velit laoreet id donec. Neque
+        vitae tempus quam pellentesque nec nam. Semper eget duis at tellus at
+        urna condimentum mattis pellentesque. Faucibus vitae aliquet nec
+        ullamcorper sit. Aliquam vestibulum morbi blandit cursus risus at
+        ultrices. Malesuada nunc vel risus commodo viverra maecenas.
+      </Text>
+      <Text>
+        Pretium viverra suspendisse potenti nullam ac tortor vitae purus
+        faucibus. Cursus eget nunc scelerisque viverra mauris in aliquam. Eu
+        scelerisque felis imperdiet proin fermentum leo. At risus viverra
+        adipiscing at. Sed risus pretium quam vulputate dignissim suspendisse
+        in. In metus vulputate eu scelerisque felis imperdiet. Viverra ipsum
+        nunc aliquet bibendum. Elementum integer enim neque volutpat ac
+        tincidunt vitae semper quis. Nulla facilisi etiam dignissim diam quis.
+        Consequat semper viverra nam libero justo laoreet. Quis blandit turpis
+        cursus in hac habitasse. Rhoncus mattis rhoncus urna neque viverra.
+        Vitae auctor eu augue ut lectus arcu bibendum at varius. Enim lobortis
+        scelerisque fermentum dui faucibus in ornare quam. Aliquet sagittis id
+        consectetur purus ut. Feugiat pretium nibh ipsum consequat nisl vel.
+        Malesuada fames ac turpis egestas sed tempus urna et. Bibendum est
+        ultricies integer quis auctor elit. Velit laoreet id donec ultrices
+        tincidunt arcu. Ac feugiat sed lectus vestibulum mattis. Sit amet
+        consectetur adipiscing elit pellentesque habitant. Vestibulum sed arcu
+        non odio euismod. Sed viverra tellus in hac habitasse platea dictumst
+        vestibulum. Vel turpis nunc eget lorem dolor sed viverra.
+      </Text>
     </main>
   );
 }
